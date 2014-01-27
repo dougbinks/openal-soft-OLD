c8166c12c955c78620fe3b1c68e1100832c620b0
Doug Binks <doug@enkisoftware.com>
2014-01-27 23:04:05 +0100

    Added sample code to play back a square wave at half a period offset from another, thus cancelling it.

--- examples/aldeviceclock.c
+++ examples/aldeviceclock.c
@@ -1,7 +1,7 @@
 /*
- * OpenAL Source Latency Example
+ * OpenAL Source Device Clock Example
  *
- * Copyright (c) 2012 by Chris Robinson <chris.kcat@gmail.com>
+ * Copyright (c) 2014 by Doug Binks & Chris Robinson <chris.kcat@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -22,7 +22,9 @@
  * THE SOFTWARE.
  */
 
-/* This file contains an example for checking the latency of a sound. */
+/* This file contains an example for synchronising two sounds. */
+/* It should play a square wave followed by silence, */
+/* as a second square wave is played inverted on top. */
 
 #include <stdio.h>
 #include <assert.h>
@@ -34,6 +36,7 @@
 
 
 static LPALGETSOURCEI64VSOFT alGetSourcei64vSOFT;
+static LPALGETSOURCEI64VSOFT alSourcei64SOFT;
 
 #define WAVEHALFPERIOD 256
 #define BUFFSIZE       102400
@@ -74,12 +77,16 @@ int OpenAL(void)
 int main(int argc, char **argv)
 {
     ALint64SOFT clockInfo[4];
+    ALint64SOFT clockInfo2nd[4];
+    ALint64SOFT clockToPlay;
     ALenum state;
+    ALenum state2nd;
     ALenum error;
     ALCint freq;
     ALuint Buffer;
     ALuint Sources[2];
     float  BuffSource[BUFFSIZE];
+    int PlayingSecond;
 
 
 
@@ -115,6 +122,7 @@ int main(int argc, char **argv)
     /* Define a macro to help load the function pointers. */
 #define LOAD_PROC(x)  ((x) = alGetProcAddress(#x))
     LOAD_PROC(alGetSourcei64vSOFT);
+    LOAD_PROC(alSourcei64SOFT);
 #undef LOAD_PROC
 
     /* Load the sound into a buffer. */
@@ -132,22 +140,34 @@ int main(int argc, char **argv)
     
     alcGetIntegerv( pALDevice, ALC_FREQUENCY, 1, &freq );
     alBufferData( Buffer, AL_FORMAT_MONO_FLOAT32, BuffSource, sizeof( BuffSource ), freq );
-    alSourcei(Sources[0], AL_SOURCE_RELATIVE, AL_TRUE);
-    alSourcei(Sources[0], AL_LOOPING, AL_FALSE);
-    alSourcei(Sources[0], AL_BUFFER, Buffer);
+    for( int b = 0; b < 2; ++b )
+    {
+        alSourcei(Sources[b], AL_SOURCE_RELATIVE, AL_TRUE);
+        alSourcei(Sources[b], AL_LOOPING, AL_FALSE);
+        alSourcei(Sources[b], AL_BUFFER, Buffer);
+    }
 
 
     /* Play the sound until it finishes. */
     alSourcePlay(Sources[0]);
+    PlayingSecond = 0;
+    printf("\rSource, Offset, OutputSampleCount, Frequency, UpdateSize\n" );
     do {
         Sleep(10);
         alGetSourcei(Sources[0], AL_SOURCE_STATE, &state);
 
-        /* Get the source offset and latency. AL_SEC_OFFSET_LATENCY_SOFT will
-         * place the offset (in seconds) in offsets[0], and the time until that
-         * offset will be heard (in seconds) in offsets[1]. */
         alGetSourcei64vSOFT(Sources[0], AL_SAMPLE_OFFSET_DEVICE_CLOCK_SOFT, clockInfo);
-        printf("\rOffset: %ld, OutputSampleCount:%ld, Frequency:%ld, UpdateSize:%ld\n", clockInfo[0], clockInfo[1], clockInfo[2], clockInfo[3] );
+        alGetSourcei64vSOFT(Sources[1], AL_SAMPLE_OFFSET_DEVICE_CLOCK_SOFT, clockInfo2nd);
+        if( clockInfo[0] + 2*WAVEHALFPERIOD > BUFFSIZE / 4 && !PlayingSecond )
+        {
+            // want to play at a point where the second wave cancels first
+            clockToPlay = clockInfo[1] - ( clockInfo[0] % (2 * WAVEHALFPERIOD) ) + ( 5 * WAVEHALFPERIOD );
+            alSourcei64SOFT( Sources[1], AL_PLAY_ON_DEVICE_CLOCK_SOFT, clockToPlay );
+            alSourcePlay(Sources[1]);
+            PlayingSecond = 1;
+        }
+        printf("0, %ld, %ld, %ld, %ld\n", clockInfo[0], clockInfo[1], clockInfo[2], clockInfo[3] );
+        printf("1, %ld, %ld, %ld, %ld\n", clockInfo2nd[0], clockInfo2nd[1], clockInfo2nd[2], clockInfo2nd[3] );
         fflush(stdout);
         error = alGetError();
 
